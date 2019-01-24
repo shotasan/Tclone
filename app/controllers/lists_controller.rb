f541ab9532417452dc111135efbe719b15a2621d
shotasan <wondermoai@gmail.com>
2019-01-24 15:58:43 +0900

    削除機能を作成

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -20,6 +20,12 @@ class ListsController < ApplicationController
     @list = List.new(list_params)
   end
 
+  def destroy
+    @list = List.find_by(id: params[:id])
+    @list.destroy
+    redirect_to lists_path
+  end
+
   private
 
   def list_params
