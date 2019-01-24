7c5d3a4b74576e5031113e9a1e3e7eacf1e60e51
shotasan <wondermoai@gmail.com>
2019-01-24 17:29:16 +0900

    編集機能を作成

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -26,6 +26,16 @@ class ListsController < ApplicationController
     redirect_to lists_path
   end
 
+  def edit
+    @list = List.find_by(id: params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+    @list.update(list_params)
+    redirect_to lists_path
+  end
+
   private
 
   def list_params
